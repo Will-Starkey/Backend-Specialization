c5b7be8d0cc6ea0c3c42409ea07ed85db05322b9
Will Starkey <williamvstarkey@gmail.com>
2026-04-27 08:43:40 -0400

    Swagger and Unittest

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Dragonfly@localhost/mechanics_db'
     DEBUG = True
@@ -14,3 +16,8 @@ class TestingConfig:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
     DEBUG = True
     CACHE_TYPE = 'SimpleCache'
+    
+
+class ProductionConfig:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI)
+    CACHE_TYPE = "SimpleCache"
